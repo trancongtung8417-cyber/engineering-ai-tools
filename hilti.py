cb8c5ecc854472f20e0b1330f6616ddeac687d96
trancongtung8417-cyber <trancong.tung8417@gmail.com>
2026-03-26 21:22:57 +0700

    Add files via upload

--- hilti.py
+++ hilti.py
@@ -1,19 +1,11 @@
 import streamlit as st
+import time
 from supabase import create_client, Client
 from fpdf import FPDF
 import os
 
 # --- 1. CẤU HÌNH TRANG ---
 st.set_page_config(page_title="Hilti - Biên Bản", page_icon="🛠️", layout="centered")
-
-# --- 2. KẾT NỐI SUPABASE ---
-try:
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    supabase: Client = create_client(url, key)
-except Exception as e:
-    st.error(f"⚠️ Lỗi cấu hình Secrets: {e}")
-    st.stop()
 
 # --- 3. CSS GIAO DIỆN (CĂN ĐẦU HÀNG, BỎ KHUNG MỜ) ---
 st.markdown("""
@@ -82,6 +74,15 @@ st.markdown("""
     }
     </style>
     """, unsafe_allow_html=True)
+
+# --- 2. KẾT NỐI SUPABASE ---
+try:
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    supabase: Client = create_client(url, key)
+except Exception as e:
+    st.error(f"⚠️ Lỗi cấu hình Secrets: {e}")
+    st.stop()
 
 # --- 4. HÀM TẠO PDF (ĐÃ SỬA LỖI SET_FONT) ---
 def generate_pdf(data):
